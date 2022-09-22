8d04d725ed950aed3b5b88766a90e0cfcef8e1a5
Danial Mazarei <71553400+dmazarei@users.noreply.github.com>
2022-09-22 14:51:10 +0330

    Update plot.R
    add new plot

--- plot.R
+++ plot.R
@@ -8,4 +8,13 @@ for (i in 1:length(unique(x$Datetime))) {
 }
 abline(v=length(x$Close),lty=2,lwd=3,)
 
-######
+######  8 function observation
+
+a_n <- which(x$Datetime==unique(x$Datetime)[1])
+a_n[length(a_n)]
+plot(x$Close[a_n[1]:a_n[length(a_n)]],type="l",ylim=c(242,270), lwd=3,xaxt='n',ylab = "price",xlab = "")
+for (i in 2:length(unique(x$Datetime))) {
+  a_n <- which(x$Datetime==unique(x$Datetime)[i])
+  lines(x$Close[a_n[1]:a_n[length(a_n)]], lty=i, lwd=3)
+}
+
